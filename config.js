572948da75401ac9f2077156ba6316a83c26a74d
KovezPlay <156551158+KovezPlay@users.noreply.github.com>
2024-06-09 22:01:42 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    token: "TOKEN-DO-BOT",
+    id: "ID-DO-BOT",
+    secret: "CLIENT-SECRET",
+    webhook: "WEBHOOK-LINK",
+    prefix: ";",
+    owners: [""],
+    authLink: "https://discord.com/api/oauth2/authorize?client_id=ID-DO-BOT&redirect_uri=LINK-AQUI&response_type=code&scope=identify%20guilds.join",
+
+}
